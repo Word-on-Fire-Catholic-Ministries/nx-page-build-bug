c9ce2fd8c03281496440675f1d3a8407598a931b
Sean Davis <sdavis@wordonfire.org>
2023-11-08 11:43:52 -0600

    Bug fixed state

---
+++ apps/bugapp/app/test/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable-next-line */
+export interface TestProps {}
+
+export default function Test(props: TestProps) {
+  return (
+    <div>
+      <h1>Welcome to Test!</h1>
+    </div>
+  );
+}
